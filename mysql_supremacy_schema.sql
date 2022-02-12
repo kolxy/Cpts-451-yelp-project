ab92208b654c0219e4e7f0a94ed5d1a2d1b6e52a
Max <kolxy@qq.com>
2022-02-11 20:19:03 -0800

    final

--- mysql_supremacy_schema.sql
+++ mysql_supremacy_schema.sql
@@ -77,6 +77,10 @@ CREATE TABLE "tips" (
 CREATE TABLE  "checkin" (
   "business_id" varchar(22),
   "timestamp" timestamp(6),
+  "year" int8,
+  "month" int8,
+  "day" int8,
+  "clock_time" time,
   PRIMARY KEY ("business_id", "timestamp"),
   FOREIGN KEY ("business_id") REFERENCES "business"("business_id")
   ON DELETE CASCADE
