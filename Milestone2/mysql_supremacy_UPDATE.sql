044f827dbfb0023b3b5df8d93a886541132a8b64
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2022-03-27 15:57:23 -0700

    part3 updates done, need peer review

--- Milestone2/mysql_supremacy_UPDATE.sql
+++ Milestone2/mysql_supremacy_UPDATE.sql
@@ -6,7 +6,7 @@ from (
 	from business
 	natural join checkin
 	group by business_id ) as cnt
-where business.business_id = cnt.business_id
+where business.business_id = cnt.business_id;
 
 -- update business tip cnt
 update business
@@ -16,7 +16,28 @@ from (
 	from business
 	natural join tips
 	group by business.business_id) as cnt
-where business.business_id = cnt.business_id
+where business.business_id = cnt.business_id;
+
+
+-- update the_user totallikes cnt
+update the_user
+set total_likes = cnt.likes_cnt
+from (
+	select user_id, sum(likes) as likes_cnt
+	from the_user
+	natural join tips
+	group by user_id) as cnt
+where the_user.user_id = cnt.user_id;
+
+-- update the_user tip_count
+update the_user
+set tip_count = cnt.tipcnt
+from (select user_id, count(user_id) as tipcnt
+	  from the_user
+	  natural join tips
+	  group by user_id) as cnt
+where the_user.user_id = cnt.user_id;
+
 
 
 
