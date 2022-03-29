1fd8dbee3a39405c866c35f0637f0c1dce71a573
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2022-03-28 23:09:50 -0700

    done part 6 a-d

--- YelpSrc/YelpMain/MainWindow.xaml.cs
+++ YelpSrc/YelpMain/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YelpQueryEngine;
+using Npgsql;
 
 namespace YelpMain
 {
@@ -23,6 +25,83 @@ namespace YelpMain
         public MainWindow()
         {
             InitializeComponent();
+            initState();
         }
+
+
+        private void initState()
+        {
+            string sqlstr = "SELECT distinct state FROM business ORDER BY state";
+            Utils.executeQuery(sqlstr, addStateUI);
+        }
+
+        private void addStateUI(NpgsqlDataReader reader)
+        {
+            stateList.Items.Add(reader.GetString(0));
+        }
+
+        /// <summary>
+        /// State selection change event.
+        /// Display corresponding cities of selected state.
+        /// </summary>
+        /// <param name="sender"> obj. </param>
+        /// <param name="e">event. </param>
+        private void stateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cityList.Items.Clear();
+            if (stateList.SelectedIndex < 0)
+            {
+                return;
+            }
+            string sqlstr = $"SELECT distinct city FROM business WHERE state = '{stateList.SelectedItem.ToString()}' ORDER BY city";
+            Utils.executeQuery(sqlstr, addCityUI);
+        }
+
+        /// <summary>
+        /// <Helper> Add city from query reader.
+        /// </summary>
+        /// <param name="reader"></param>
+        private void addCityUI(NpgsqlDataReader reader)
+        {
+            cityList.Items.Add(reader.GetString(0));
+        }
+
+        private void cityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            zipList.Items.Clear();
+            if (cityList.SelectedIndex < 0)
+            {
+                return;
+            }
+            string sqlstr = $"SELECT distinct zipcode from business WHERE state = '{stateList.SelectedItem.ToString()}' AND city = '{cityList.SelectedItem.ToString()}' ORDER BY zipcode";
+            Utils.executeQuery(sqlstr, addZipCodeUI);
+        }
+        private void addZipCodeUI(NpgsqlDataReader reader)
+        {
+            zipList.Items.Add(reader.GetString(0));
+        }
+
+
+        private void zipList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            businessCatList.Items.Clear();
+            if (zipList.SelectedIndex < 0)
+            {
+                return;
+            }
+            string sqlstr = $"select distinct business_category.name from business, business_category where" +
+                $" state = '{stateList.SelectedItem.ToString()}' and city = '{cityList.SelectedItem.ToString()}'" +
+                $" and zipcode = '{zipList.SelectedItem.ToString()}' and business.business_id = business_category.business_id";
+            Utils.executeQuery(sqlstr, addBusinessCat);
+        }
+
+        private void addBusinessCat(NpgsqlDataReader reader)
+        {
+            businessCatList.Items.Add(reader.GetString(0));
+        }
+
+         
+
+         
     }
 }
---
+++ YelpSrc/YelpQueryEngine/Utils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Npgsql;
+
+namespace YelpQueryEngine
+{
+    public class Utils
+    {
+        public static string buildConnectionStr()
+        {
+            return "Host = localhost; Username = postgres; Database = yelp451; password=123456";
+        }
+
+        /// <summary>
+        /// Helper for query steps.
+        /// </summary>
+        /// <param name="sqlstr">sql statement</param>
+        /// <param name="myf">delagate function </param>
+        public static void executeQuery(string sqlstr, Action<NpgsqlDataReader> myf)
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionStr()))
+            {
+                connection.Open();
+
+                // command object for queries.
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+
+                    // set sqlstr to cmd attribute
+                    cmd.CommandText = sqlstr;
+
+                    // try catch handler when perform sql query.
+                    try
+                    {
+                        // reader object that holds the collection of data from query.
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            // function to do with data
+                            myf(reader);
+                        }
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        Console.WriteLine(ex.Message.ToString());
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+    }
+}
