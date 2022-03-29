c2adbf92ca69ed978f7e5ae250e46498d72bc5f0
Boxiang Lin <57877290+boxianglin@users.noreply.github.com>
2022-03-29 14:29:26 -0700

    search result in progress

--- YelpSrc/YelpMain/MainWindow.xaml.cs
+++ YelpSrc/YelpMain/MainWindow.xaml.cs
@@ -26,9 +26,12 @@ namespace YelpMain
         {
             InitializeComponent();
             initState();
+            initBusinessInfoTableHeader();
         }
 
-
+        /// <summary>
+        /// Init the states available in db.
+        /// </summary>
         private void initState()
         {
             string sqlstr = "SELECT distinct state FROM business ORDER BY state";
@@ -38,6 +41,23 @@ namespace YelpMain
         private void addStateUI(NpgsqlDataReader reader)
         {
             stateList.Items.Add(reader.GetString(0));
+        }
+
+        /// <summary>
+        /// Init the header row for [businessInfoTable]
+        /// </summary>
+        private void initBusinessInfoTableHeader()
+        {
+            Dictionary<string, string> colNameBinding = Constants.getBusinessInfoTableHeaderNameAndBinder();
+            Dictionary<string, int> colNameWidth = Constants.getBusinessInfoTableHeaderColWidth();
+            foreach (var item in colNameBinding)
+            {
+                DataGridTextColumn col = new DataGridTextColumn();
+                col.Header = item.Key;
+                col.Binding = new Binding(item.Value);
+                col.Width = colNameWidth[item.Key];
+                businessInfoTable.Columns.Add(col);
+            }
         }
 
         /// <summary>
@@ -57,15 +77,17 @@ namespace YelpMain
             Utils.executeQuery(sqlstr, addCityUI);
         }
 
-        /// <summary>
-        /// <Helper> Add city from query reader.
-        /// </summary>
-        /// <param name="reader"></param>
         private void addCityUI(NpgsqlDataReader reader)
         {
             cityList.Items.Add(reader.GetString(0));
         }
 
+
+        /// <summary>
+        /// Event that to display the zip codes when selected a city from [cityList]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void cityList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             zipList.Items.Clear();
@@ -82,6 +104,11 @@ namespace YelpMain
         }
 
 
+        /// <summary>
+        /// Event that to display business category when select a zip code from [zipList]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void zipList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             businessCatList.Items.Clear();
@@ -100,8 +127,76 @@ namespace YelpMain
             businessCatList.Items.Add(reader.GetString(0));
         }
 
-         
+        /// <summary>
+        /// Add category to the [selectedCatList] from [businessCatList]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addCatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (businessCatList.SelectedIndex < 0)
+            {
+                return;
+            }
+            selectedCatList.Items.Add(businessCatList.SelectedItem.ToString());
+        }
 
-         
+        /// <summary>
+        /// Remove category from the [selectedCatList]
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void removeCatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedCatList.SelectedIndex < 0)
+            {
+                return;
+            }
+            selectedCatList.Items.Remove(selectedCatList.SelectedItem.ToString());
+        }
+
+        /// <summary>
+        /// Event that click the search button, should that the business info on [businessInfoTable].
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void searchBusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            if (selectedCatList.SelectedIndex < 0)
+            {
+                return;
+            }
+            sb.Append($"select * from business where" +
+                $" state = '{stateList.SelectedItem.ToString()}' and city = '{cityList.SelectedItem.ToString()}'" +
+                $" and zipcode = '{zipList.SelectedItem.ToString()}' and business.business_id in (select business_id from business_category where ");
+            
+            foreach (var item in selectedCatList.Items){
+                sb.Append($" '{item.ToString()}') ");
+            }
+
+            string sqlstr = sb.ToString();
+            Utils.executeQuery(sqlstr, showSearchResult);
+        }
+
+        private void showSearchResult(NpgsqlDataReader reader)
+        {
+            Business bus = new Business();
+            bus.business_id = reader.GetString(0);
+            bus.name = reader.GetString(1);
+            bus.city = reader.GetString(2);
+            bus.state = reader.GetString(3);
+            bus.zipcode = reader.GetString(4);
+            bus.address = reader.GetString(5);
+            bus.starts = reader.GetFloat(6);
+            bus.latitude = reader.GetDouble(7);
+            bus.longitude = reader.GetDouble(8);
+            bus.num_checkings = reader.GetInt64(9);
+            bus.num_tips = reader.GetInt64(10);
+            bus.is_open = reader.GetBoolean(11);
+            businessInfoTable.Items.Add(bus);
+
+        }
     }
 }
---
+++ YelpSrc/YelpQueryEngine/Business.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YelpQueryEngine
+{
+    public class Business
+    {
+        public string business_id { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string address { get; set; }
+        public float starts { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public Int64 num_checkings { get; set; }
+        public Int64 num_tips { get; set; }
+        public bool is_open { get; set; }
+    }
+}
