fb5782b4572b6a6875b1a4414a87aab94919cd48
Max <kolxy@qq.com>
2022-03-31 17:25:09 -0700

    example adjust

--- Example/CptS451_milestone2_demo.sql
+++ Example/CptS451_milestone2_demo.sql
@@ -1,14 +1,14 @@
 
 --GLOSSARY
 --table names
-businesstable
-usertable
-tipstable
-friendstable
+business
+the_user
+tips
+user_follow
 checkin
-businesscategory
-businessattribute
-businesshours
+business_category
+business_attribute
+business_hours
 
 --some attribute names
 zipcode
@@ -17,72 +17,72 @@ city  (business city)
 name   (business name)
 user_id
 friend_id
-numtips
-numCheckins
+num_tips
+num_checkings
 
 user_id
-tipcount  (user)
-totallikes (user)
+tip_count  (user)
+total_likes (user)
 
-tipdate
-tiptext
+timestamp
+text
 likes  (tip)
 
-checkindate
+timestamp
 
 
 --1.
 SELECT COUNT(*) 
-FROM  businesstable;
+FROM  business;
 SELECT COUNT(*) 
-FROM  usertable;
+FROM  the_user;
 SELECT COUNT(*) 
-FROM  tipstable;
+FROM  tips;
 SELECT COUNT(*) 
-FROM  friendstable;
+FROM  user_follow;
 SELECT COUNT(*) 
 FROM  checkin;
 SELECT COUNT(*) 
-FROM  businesscategory;
+FROM  business_category;
 SELECT COUNT(*) 
-FROM  businessattribute;
+FROM  business_attribute;
 SELECT COUNT(*) 
-FROM  businesshours;
+FROM  business_hours;
 
 
 
 --2. Run the following queries on your business table, checkin table and review table. Make sure to change the attribute names based on your schema. 
 
-SELECT zipcode, COUNT(distinct C.category)
-FROM businesstable as B, businesscategory as C
+SELECT zipcode, COUNT(distinct C.name)
+FROM business as B, business_category as C
 WHERE B.business_id = C.business_id
 GROUP BY zipcode
-HAVING count(distinct C.category)>300
+HAVING count(distinct C.name)>300
 ORDER BY zipcode;
 
-SELECT zipcode, COUNT(distinct A.attribute)
-FROM businesstable as B, businessattribute as A
+SELECT zipcode, COUNT(distinct A.name)
+FROM business as B, business_attribute as A
 WHERE B.business_id = A.business_id
 GROUP BY zipcode
-HAVING count(distinct A.attribute) = 30;
+HAVING count(distinct A.name) = 30;
 
 
-SELECT usertable.user_id, count(friend_id)
-FROM usertable, friendstable
-WHERE usertable.user_id = friendstable.user_id AND 
-      usertable.user_id = 'NxtYkOpXHSy7LWRKJf3z0w'
-GROUP BY usertable.user_id;
+SELECT the_user.user_id, count(friend_id)
+FROM the_user, user_follow
+WHERE the_user.user_id = user_follow.user_id AND 
+      the_user.user_id = 'NxtYkOpXHSy7LWRKJf3z0w'
+GROUP BY the_user.user_id;
 
 
 --3. Run the following queries on your business table, checkin table and tips table. Make sure to change the attribute names based on your schema. 
 
 
-SELECT business_id, name, city, numtips, numCheckins
-FROM businesstable 
+SELECT business_id, name, city, num_tips, num_checkings
+FROM business 
 WHERE business_id ='K8M3OeFCcAnxuxtTc0BQrQ';
 
-SELECT user_id, name, tipcount, totallikes
-FROM usertable
+SELECT user_id, name, tip_count, likes
+FROM the_user
 WHERE user_id = 'NxtYkOpXHSy7LWRKJf3z0w';
 
 -----------
@@ -92,17 +92,18 @@ FROM checkin
 WHERE business_id ='K8M3OeFCcAnxuxtTc0BQrQ';
 
 SELECT count(*)
-FROM tipstable
+FROM tips
 WHERE  business_id = 'K8M3OeFCcAnxuxtTc0BQrQ';
 
 
 --4. 
 --Type the following statements. Make sure to change the attribute names based on your schema. 
 
-SELECT business_id,name, city, numCheckins, numtips
-FROM businesstable 
+SELECT business_id,name, city, num_checkings, num_tips
+FROM business 
 WHERE business_id ='hDD6-yk1yuuRIvfdtHsISg';
 
+-- HAVE PROBLEMS
 INSERT INTO checkin (business_id, checkindate)
 VALUES ('hDD6-yk1yuuRIvfdtHsISg','2022-03-31 10:45:07');
 
@@ -111,22 +112,22 @@ VALUES ('hDD6-yk1yuuRIvfdtHsISg','2022-03-31 10:45:07');
 --Type the following statements. Make sure to change the attribute names based on your schema.  
 
 -- same as the above query in part4
-SELECT business_id,name, city, numCheckins, numtips
-FROM businesstable 
+SELECT business_id,name, city, num_checkings, num_tips
+FROM business 
 WHERE business_id ='hDD6-yk1yuuRIvfdtHsISg';
 
-SELECT user_id, name, tipcount, totallikes
-FROM usertable
+SELECT user_id, name, tip_count, total_likes
+FROM the_user
 WHERE user_id = '3z1EttCePzDn9OZbudD5VA';
 
 
-INSERT INTO tipstable (user_id, business_id, tipdate, tiptext,likes)  
+INSERT INTO tips (user_id, business_id, timestamp, tiptext,likes)  
 VALUES ('3z1EttCePzDn9OZbudD5VA','hDD6-yk1yuuRIvfdtHsISg', '2022-03-31 13:00','EVERYTHING IS AWESOME',0);
 
-UPDATE tipstable 
+UPDATE tips 
 SET likes = likes+1
 WHERE user_id = '3z1EttCePzDn9OZbudD5VA' AND 
       business_id = 'hDD6-yk1yuuRIvfdtHsISg' AND 
-      tipdate ='2022-03-31 13:00';
+      timestamp ='2022-03-31 13:00';
 
       
